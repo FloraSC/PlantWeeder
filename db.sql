df15faecadb8196cc453148ca5d54f0403d91c87
Roopal Kondepudi <roopal.kondepudi@gmail.com>
2019-10-27 11:59:10 -0700

    database creation script

--- db.sql
+++ db.sql
@@ -1,4 +1,6 @@
+DROP TABLE IF EXISTS native_plants_location;
 DROP TABLE IF EXISTS native_plants;
+
 CREATE TABLE IF NOT EXISTS native_plants (
   id INT(11) PRIMARY KEY AUTO_INCREMENT,
   scientific_name VARCHAR(256) UNIQUE NOT NULL,
@@ -7,11 +9,11 @@ CREATE TABLE IF NOT EXISTS native_plants (
   url VARCHAR(512)
 );
 
-DROP TABLE IF EXISTS native_plants_location;
 CREATE TABLE IF NOT EXISTS native_plants_location (
   id INT(11) PRIMARY KEY AUTO_INCREMENT,
-  scientific_name VARCHAR(256) REFERENCES native_plants(scientific_name),
+  scientific_name VARCHAR(256),
   latitude INT(11) NOT NULL,
   longitude INT(11) NOT NULL,
   image_url VARCHAR(512),
+  FOREIGN KEY (scientific_name) REFERENCES native_plants(scientific_name)
 );
