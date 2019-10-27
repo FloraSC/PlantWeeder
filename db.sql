abd32e3ab93db3d10d3c30893be79c1d1c00e662
Micah Steinberg <mbsteinb@usc.edu>
2019-10-27 11:51:56 -0700

    Updated db script

--- db.sql
+++ db.sql
@@ -1,16 +1,17 @@
+DROP TABLE IF EXISTS native_plants;
 CREATE TABLE IF NOT EXISTS native_plants (
-  id PRIMARY KEY AUTO_INCREMENT,
-  scientific_name TEXT UNIQUE NOT NULL,
-  common_name TEXT,
-  description MEDIUMTEXT,
-  url TEXT
-)
+  id INT(11) PRIMARY KEY AUTO_INCREMENT,
+  scientific_name VARCHAR(256) UNIQUE NOT NULL,
+  common_name VARCHAR(256),
+  description VARCHAR(2048),
+  url VARCHAR(512)
+);
 
+DROP TABLE IF EXISTS native_plants_location;
 CREATE TABLE IF NOT EXISTS native_plants_location (
   id INT(11) PRIMARY KEY AUTO_INCREMENT,
-  scientific_name TEXT,
-  latituce INT(11) NOT NULL,
+  scientific_name VARCHAR(256) REFERENCES native_plants(scientific_name),
+  latitude INT(11) NOT NULL,
   longitude INT(11) NOT NULL,
-  image_url TEXT,
-  fk_npl_scientific_name FOREIGN KEY (scientific_name) REFERENCES native_plants(scientific_name)
-)
+  image_url VARCHAR(512),
+);
